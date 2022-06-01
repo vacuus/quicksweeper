d46f5eaf870b9bffaf15b7316183defe533c3b6d
Eric Yu <zbrachinara@gmail.com>
2022-06-01 11:36:18 -0400

    extracted neighbor code to the position struct

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,77 @@
+use std::mem::MaybeUninit;
 
 #[derive(PartialEq, Clone, Debug)]
 pub struct Position(pub u32, pub u32);
+
+pub struct PositionNeighborsIter {
+    items: [Position; 8],
+    size: u8,
+}
+
+impl Iterator for PositionNeighborsIter {
+    type Item = Position;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        (self.size != 0).then(|| {
+            self.size -= 1;
+            self.items[self.size as usize].clone()
+        })
+    }
+}
+
+impl Position {
+    pub fn iter_neighbors(&self, max_x: u32, max_y: u32) -> PositionNeighborsIter {
+        let (max_x, max_y) = (max_x - 1, max_y - 1);
+        let mut size = 0;
+        let mut items: [Position; 8] = unsafe { MaybeUninit::uninit().assume_init() };
+
+        // left
+        if self.0 != 0 {
+            // lower left
+            if self.1 != 0 {
+                items[size] = Position(self.0 - 1, self.1 - 1);
+                size += 1;
+            }
+            // upper left
+            if self.1 != max_y {
+                items[size] = Position(self.0 - 1, self.1 + 1);
+                size += 1;
+            }
+            items[size] = Position(self.0 - 1, self.1);
+            size += 1;
+        }
+        // right
+        if self.0 != max_x {
+            // lower right
+            if self.1 != 0 {
+                items[size] = Position(self.0 + 1, self.1 - 1);
+                size += 1;
+            }
+            // upper right
+            if self.1 != max_y {
+                items[size] = Position(self.0 + 1, self.1 + 1);
+                size += 1;
+            }
+            items[size] = Position(self.0 + 1, self.1);
+            size += 1;
+        }
+        // bottom
+        if self.1 != 0 {
+            items[size] = Position(self.0, self.1 - 1);
+            size += 1;
+        }
+        // top
+        if self.1 != max_y {
+            items[size] = Position(self.0, self.1 + 1);
+            size += 1;
+        }
+
+        PositionNeighborsIter {
+            items,
+            size: size as u8,
+        }
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct CheckCell(pub Position);
--- src/minefield/field.rs
+++ src/minefield/field.rs
@@ -1,7 +1,4 @@
-use std::{
-    mem::MaybeUninit,
-    ops::{Index, IndexMut},
-};
+use std::ops::{Index, IndexMut};
 
 use array2d::Array2D;
 use bevy::prelude::*;
@@ -28,83 +25,13 @@ struct Mine(Position);
 #[derive(Deref, DerefMut, Component)]
 pub struct Minefield(pub(super) Array2D<MineCell>);
 
-struct MineNeighbors<'a> {
-    backing: &'a Minefield,
-    items: [Position; 8],
-    size: u8,
-}
-
-impl<'a> Iterator for MineNeighbors<'a> {
-    type Item = (Position, &'a MineCell);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        (self.size != 0).then(|| {
-            self.size -= 1;
-            let pos = std::mem::replace(&mut self.items[self.size as usize], unsafe {
-                MaybeUninit::uninit().assume_init()
-            });
-            (pos.clone(), &self.backing[pos])
-        })
-    }
-}
-
 impl Minefield {
     pub fn iter_neighbors_enumerated(
         &self,
         pos: Position,
     ) -> impl Iterator<Item = (Position, &MineCell)> + '_ {
-        let mut size = 0;
-        let mut items: [Position; 8] = unsafe { MaybeUninit::uninit().assume_init() };
-
-        let max_x = (self.num_columns() - 1) as u32;
-        let max_y = (self.num_rows() - 1) as u32;
-
-        // left
-        if pos.0 != 0 {
-            // lower left
-            if pos.1 != 0 {
-                items[size] = Position(pos.0 - 1, pos.1 - 1);
-                size += 1;
-            }
-            // upper left
-            if pos.1 != max_y {
-                items[size] = Position(pos.0 - 1, pos.1 + 1);
-                size += 1;
-            }
-            items[size] = Position(pos.0 - 1, pos.1);
-            size += 1;
-        }
-        // right
-        if pos.0 != max_x {
-            // lower right
-            if pos.1 != 0 {
-                items[size] = Position(pos.0 + 1, pos.1 - 1);
-                size += 1;
-            }
-            // upper right
-            if pos.1 != max_y {
-                items[size] = Position(pos.0 + 1, pos.1 + 1);
-                size += 1;
-            }
-            items[size] = Position(pos.0 + 1, pos.1);
-            size += 1;
-        }
-        // bottom
-        if pos.1 != 0 {
-            items[size] = Position(pos.0, pos.1 - 1);
-            size += 1;
-        }
-        // top
-        if pos.1 != max_y {
-            items[size] = Position(pos.0, pos.1 + 1);
-            size += 1;
-        }
-
-        MineNeighbors {
-            backing: &self,
-            items,
-            size: size as u8,
-        }
+        pos.iter_neighbors(self.num_columns() as u32, self.num_rows() as u32)
+            .map(|pos| (pos.clone(), &self[pos]))
     }
 }
 
