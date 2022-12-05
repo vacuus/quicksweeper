936a0076498f518a0090e84a10acd6337ce6622f
zbrachinara <zbrachinara@gmail.com>
2022-12-05 12:43:34 -0500

    remove unused DAS procedures for cursor

--- src/cursor.rs
+++ src/cursor.rs
@@ -1,150 +1,7 @@
-use crate::common::{CheckCell, Direction, FlagCell, InitCheckCell, Position};
+use crate::common::{CheckCell, FlagCell, InitCheckCell, Position};
 use crate::singleplayer::minefield::specific::{MineCellState, CELL_SIZE};
 use crate::singleplayer::minefield::Minefield;
 use bevy::{prelude::*, render::camera::Camera};
-use gridly::prelude::*;
-use tap::Tap;
-
-#[derive(Debug)]
-struct KeyTimers {
-    key_left: HoldTimer,
-    key_right: HoldTimer,
-    key_up: HoldTimer,
-    key_down: HoldTimer,
-}
-
-impl KeyTimers {
-    fn with_bindings(activate_duration: f32, hold_delay: f32, bindings: &Bindings) -> Self {
-        Self {
-            key_left: HoldTimer::init(bindings.left, activate_duration, hold_delay),
-            key_right: HoldTimer::init(bindings.right, activate_duration, hold_delay),
-            key_up: HoldTimer::init(bindings.up, activate_duration, hold_delay),
-            key_down: HoldTimer::init(bindings.down, activate_duration, hold_delay),
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-struct Activations {
-    left: bool,
-    right: bool,
-    up: bool,
-    down: bool,
-}
-
-impl TryFrom<Activations> for Direction {
-    type Error = ();
-
-    fn try_from(value: Activations) -> Result<Self, Self::Error> {
-        if value.up && value.down
-            || value.left && value.right
-            || !(value.up || value.down || value.left || value.right)
-        {
-            Err(())
-        } else {
-            Ok(if value.up {
-                if value.left {
-                    Direction::NorthWest
-                } else if value.right {
-                    Direction::NorthEast
-                } else {
-                    Direction::North
-                }
-            } else if value.down {
-                if value.left {
-                    Direction::SouthWest
-                } else if value.right {
-                    Direction::SouthEast
-                } else {
-                    Direction::South
-                }
-            } else if value.left {
-                Direction::West
-            } else {
-                Direction::East
-            })
-        }
-    }
-}
-
-impl Default for KeyTimers {
-    fn default() -> Self {
-        Self::init(0.25, 0.05)
-    }
-}
-
-#[derive(Debug)]
-struct HoldTimer {
-    key: KeyCode,
-    activate_timer: Timer,
-    hold_timer: Timer,
-}
-
-impl HoldTimer {
-    fn init(key: KeyCode, activate_duration: f32, hold_delay: f32) -> Self {
-        Self {
-            key,
-            activate_timer: Timer::from_seconds(activate_duration, TimerMode::Once)
-                .tap_mut(|x| x.pause()),
-            hold_timer: Timer::from_seconds(hold_delay, TimerMode::Repeating),
-        }
-    }
-
-    fn tick(&mut self, time: &Res<Time>) {
-        self.activate_timer.tick(time.delta());
-        self.hold_timer.tick(time.delta());
-    }
-
-    fn tick_input(&mut self, time: &Res<Time>, input: &Res<Input<KeyCode>>) -> bool {
-        self.tick(time);
-
-        if input.just_released(self.key) {
-            self.activate_timer.pause();
-            self.activate_timer.reset();
-        }
-
-        let just_activated = input
-            .just_pressed(self.key)
-            .then(|| {
-                self.activate_timer.reset();
-                self.activate_timer.unpause();
-
-                self.hold_timer.reset();
-            })
-            .is_some();
-
-        self.activate_timer.finished() && self.hold_timer.just_finished() || just_activated
-    }
-}
-
-impl KeyTimers {
-    fn init(activate_duration: f32, hold_delay: f32) -> Self {
-        Self {
-            key_left: HoldTimer::init(KeyCode::A, activate_duration, hold_delay),
-            key_right: HoldTimer::init(KeyCode::D, activate_duration, hold_delay),
-            key_up: HoldTimer::init(KeyCode::W, activate_duration, hold_delay),
-            key_down: HoldTimer::init(KeyCode::S, activate_duration, hold_delay),
-        }
-    }
-
-    fn tick(&mut self, time: &Res<Time>) {
-        self.key_left.tick(time);
-        self.key_right.tick(time);
-        self.key_up.tick(time);
-        self.key_down.tick(time);
-    }
-
-    fn tick_input(&mut self, time: &Res<Time>, input: &Res<Input<KeyCode>>) -> Activations {
-        self.tick(time);
-
-        Activations {
-            left: self.key_left.tick_input(time, input),
-            right: self.key_right.tick_input(time, input),
-            up: self.key_up.tick_input(time, input),
-            down: self.key_down.tick_input(time, input),
-        }
-    }
-}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 /// The entity field describes the minefield which it is placed on
@@ -167,10 +24,6 @@ impl CursorPosition {
 }
 
 pub struct Bindings {
-    pub left: KeyCode,
-    pub right: KeyCode,
-    pub up: KeyCode,
-    pub down: KeyCode,
     pub flag: KeyCode,
     pub check: KeyCode,
 }
@@ -178,10 +31,6 @@ pub struct Bindings {
 impl Default for Bindings {
     fn default() -> Self {
         Self {
-            left: KeyCode::A,
-            right: KeyCode::D,
-            up: KeyCode::W,
-            down: KeyCode::S,
             flag: KeyCode::F,
             check: KeyCode::Space,
         }
@@ -191,7 +40,6 @@ impl Default for Bindings {
 #[derive(Component, Debug)]
 pub struct Cursor {
     position: CursorPosition,
-    timers: KeyTimers,
     check_key: KeyCode,
     flag_key: KeyCode,
 }
@@ -204,7 +52,6 @@ impl Cursor {
     pub fn new_with_keybindings(position: CursorPosition, bindings: Bindings) -> Self {
         Cursor {
             position,
-            timers: KeyTimers::with_bindings(0.25, 0.25, &bindings),
             check_key: bindings.check,
             flag_key: bindings.flag,
         }
@@ -217,31 +64,7 @@ pub fn destroy_cursors(mut commands: Commands, cursors: Query<Entity, With<Curso
         .for_each(|cursor| commands.entity(cursor).despawn())
 }
 
-pub fn move_cursor(
-    mut cursor: Query<&mut Cursor>,
-    fields: Query<&Minefield>,
-    kb: Res<Input<KeyCode>>,
-    time: Res<Time>,
-) {
-    for mut cursor in cursor.iter_mut() {
-        let activated = cursor.timers.tick_input(&time, &kb);
-
-        let Cursor {
-            position: CursorPosition(position, minefield),
-            ..
-        } = *cursor;
-        if let Some(next) = activated.try_into().ok().and_then(|direction| {
-            position.neighbor_direction(direction).and_then(|neighbor| {
-                let ent = fields.get(minefield).unwrap().get(neighbor);
-                matches!(ent, Ok(x) if x.is_some()).then(|| neighbor)
-            })
-        }) {
-            cursor.position.0 = next;
-        }
-    }
-}
-
-/// Locks the cursor to the system pointer
+/// Tracks the cursor to the system pointer
 pub fn pointer_cursor(
     windows: Res<Windows>,
     cameras: Query<(&Camera, &GlobalTransform)>,
