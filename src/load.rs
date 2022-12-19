42d34bf18f0ed768f17ab31bf2eb1fa89613e9fc
zbrachinara <zbrachinara@gmail.com>
2022-12-19 14:38:19 -0500

    fix texture sampling mode

--- src/load.rs
+++ src/load.rs
@@ -1,16 +1,13 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::texture::ImageSampler};
 use bevy_asset_loader::prelude::*;
 use iyes_loopless::prelude::AppLooplessStateExt;
 use rand::{seq::SliceRandom, Rng};
 
-use crate::{
-    main_menu::MenuState,
-    singleplayer::minefield::FieldShape,
-};
+use crate::{main_menu::MenuState, singleplayer::minefield::FieldShape};
 
 #[derive(AssetCollection, Resource)]
 pub struct Textures {
-    #[asset(texture_atlas(tile_size_x=32.0, tile_size_y=32.0, columns=4, rows=3))]
+    #[asset(texture_atlas(tile_size_x = 32.0, tile_size_y = 32.0, columns = 4, rows = 3))]
     #[asset(path = "textures.png")]
     pub mines: Handle<TextureAtlas>,
     #[asset(path = "cursor.png")]
@@ -21,6 +18,22 @@ pub struct Textures {
 pub struct Field {
     #[asset(path = "fields", collection(typed))]
     pub handles: Vec<Handle<FieldShape>>,
+}
+
+fn set_texture_mode(
+    image_assets: &mut ResMut<Assets<Image>>,
+    handle: &Handle<Image>,
+) {
+    image_assets.get_mut(handle).unwrap().sampler_descriptor = ImageSampler::nearest();
+}
+
+fn set_texture_modes(
+    texture_atlas: Res<Assets<TextureAtlas>>,
+    mut image: ResMut<Assets<Image>>,
+    textures: Res<Textures>,
+) {
+    set_texture_mode(&mut image, &texture_atlas.get(&textures.mines).unwrap().texture);
+    set_texture_mode(&mut image, &textures.cursor);
 }
 
 impl Field {
@@ -47,8 +60,9 @@ impl Plugin for ClientLoad {
             LoadingState::new(MenuState::Loading)
                 .continue_to_state(MenuState::MainMenu)
                 .with_collection::<Textures>()
-                .with_collection::<Field>()
-        );
+                .with_collection::<Field>(),
+        )
+        .add_exit_system(MenuState::Loading, set_texture_modes);
     }
 }
 
