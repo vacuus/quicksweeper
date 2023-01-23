3ea5de243a2812e49a3e11912ab8b459803bdb91
zbrachinara <zbrachinara@gmail.com>
2023-01-23 11:14:33 -0500

    tcp socket handling

--- src/server_v2/mod.rs
+++ src/server_v2/mod.rs
@@ -1,1 +1,18 @@
-pub async fn srv_main() {}
+use tokio::{runtime::Runtime, net::TcpListener};
+
+#[allow(dead_code)]
+pub fn srv_start(address: String) {
+    Runtime::new().unwrap().block_on(srv_main(address))
+}
+
+async fn srv_main(address: String) {
+    let listener = TcpListener::bind(address).await.unwrap();
+
+    while let Ok(sock) = listener.accept().await {
+        tokio::spawn(async {
+            
+        });
+    }
+}
+
+
