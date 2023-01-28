de5e6cb737937d15e6392d65986fa8c77fba6863
zbrachinara <zbrachinara@gmail.com>
2023-01-27 20:02:10 -0500

    new module for managing connection

---
+++ src/server_v2/connection.rs
@@ -0,0 +1,94 @@
+use futures_util::{Sink, SinkExt, Stream, StreamExt};
+use serde::{de::DeserializeOwned, Serialize};
+use tokio::net::TcpStream;
+use tokio_tungstenite::WebSocketStream;
+use tungstenite::Message;
+
+use crate::server::MessageError;
+
+use super::Player;
+
+pub struct Connection(pub WebSocketStream<TcpStream>);
+
+impl Connection {
+    pub async fn recv_message<D>(&mut self) -> Option<Result<D, MessageError>>
+    where
+        D: DeserializeOwned,
+    {
+        let msg = match self.0.next().await.unwrap() {
+            Ok(msg) => Some(msg),
+            Err(e) => return Some(Err(e.into())),
+        }?;
+
+        match msg {
+            Message::Ping(_) | Message::Pong(_) => None,
+            Message::Binary(v) => {
+                Some(rmp_serde::from_slice(&v).map_err(MessageError::Deserialization))
+            }
+            _ => Some(Err(MessageError::Encoding)),
+        }
+    }
+
+    pub async fn send_ser<S>(&mut self, message: S) -> Result<(), MessageError>
+    where
+        S: Serialize,
+    {
+        self.send(rmp_serde::to_vec(&message)?).await?;
+        Ok(())
+    }
+
+    pub async fn upgrade(mut self) -> Result<Player, MessageError> {
+        loop {
+            if let Some(msg) = self.recv_message().await {
+                break match msg {
+                    Ok(greeting) => Ok(Player {
+                        socket: self,
+                        info: greeting,
+                        game_channel: None,
+                    }),
+                    Err(e) => Err(e),
+                };
+            }
+        }
+    }
+}
+
+impl Stream for Connection {
+    type Item = Result<Message, tungstenite::Error>;
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        self.0.poll_next_unpin(cx)
+    }
+}
+
+impl Sink<Vec<u8>> for Connection {
+    type Error = tungstenite::Error;
+
+    fn poll_ready(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.0.poll_ready_unpin(cx)
+    }
+
+    fn start_send(mut self: std::pin::Pin<&mut Self>, item: Vec<u8>) -> Result<(), Self::Error> {
+        self.0.start_send_unpin(Message::Binary(item))
+    }
+
+    fn poll_flush(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.0.poll_flush_unpin(cx)
+    }
+
+    fn poll_close(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.0.poll_close_unpin(cx)
+    }
+}
--- src/server_v2/mod.rs
+++ src/server_v2/mod.rs
@@ -1,61 +1,23 @@
-use serde::de::DeserializeOwned;
+use futures_util::SinkExt;
+use tokio::{net::TcpListener, runtime::Runtime};
 use tokio_tungstenite as tungsten;
 
-use futures_util::{SinkExt, StreamExt};
-use tokio::{
-    net::{TcpListener, TcpStream},
-    runtime::Runtime,
+use crate::server::MessageError;
+use crate::{
+    registry::REGISTRY,
+    server::{ClientMessage, Greeting, ServerMessage},
 };
-use tungsten::WebSocketStream;
-use tungstenite::{Error, Message};
 
-use crate::server::{ClientMessage, Greeting, MessageError};
-
+use self::connection::Connection;
 use self::double_channel::DoubleChannel;
 
+mod connection;
 mod double_channel;
 
-pub fn recv_message<D>(msg: Result<Message, Error>) -> Option<Result<D, MessageError>>
-where
-    D: DeserializeOwned,
-{
-    let msg = match msg {
-        Ok(msg) => Some(msg),
-        Err(e) => return Some(Err(e.into())),
-    }?;
-
-    match msg {
-        Message::Ping(_) | Message::Pong(_) => None,
-        Message::Binary(v) => {
-            Some(rmp_serde::from_slice(&v).map_err(MessageError::Deserialization))
-        }
-        _ => Some(Err(MessageError::Encoding)),
-    }
-}
-
-struct PartialConnection(WebSocketStream<TcpStream>);
-struct Player {
-    socket: WebSocketStream<TcpStream>,
+pub struct Player {
+    socket: Connection,
     info: Greeting,
     game_channel: Option<DoubleChannel<Vec<u8>>>,
-}
-
-impl PartialConnection {
-    async fn upgrade(self) -> Result<Player, MessageError> {
-        let Self(mut sock) = self;
-        loop {
-            if let Some(msg) = recv_message(sock.next().await.unwrap()) {
-                break match msg {
-                    Ok(greeting @ Greeting { .. }) => Ok(Player {
-                        socket: sock,
-                        info: greeting,
-                        game_channel: None,
-                    }),
-                    Err(e) => Err(e),
-                };
-            }
-        }
-    }
 }
 
 impl Player {
@@ -65,28 +27,33 @@ impl Player {
             if let Some(ref mut recv) = self.game_channel {
                 tokio::select! {
                     Some(msg) = recv.recv() => {
-                        self.socket.send(Message::Binary(msg)).await;
+                        self.socket.send(msg).await;
                     }
-                    Some(maybe_msg) = self.socket.next() => {
-                        self.handle_client_message(maybe_msg)
+                    Some(maybe_msg) = self.socket.recv_message() => {
+                        self.handle_client_message(maybe_msg).await;
                     }
                 }
-            } else if let Some(msg) = self.socket.next().await {
-                self.handle_client_message(msg)
+            } else if let Some(msg) = self.socket.recv_message().await {
+                self.handle_client_message(msg).await;
             }
         }
     }
 
-    fn handle_client_message(&mut self, message: Result<Message, Error>) {
-        match recv_message(message) {
-            Some(Ok(ClientMessage::Create { game, args })) => (),
-            Some(Ok(ClientMessage::ForceLeave)) => (),
-            Some(Ok(ClientMessage::GameTypes)) => (),
-            Some(Ok(ClientMessage::Games)) => (),
-            Some(Ok(ClientMessage::Ingame { data })) => (),
-            Some(Ok(ClientMessage::Join { game })) => (),
-            Some(Err(_)) => (),
-            None => (),
+    async fn handle_client_message(&mut self, message: Result<ClientMessage, MessageError>) {
+        match message {
+            Ok(ClientMessage::Create { game, args }) => (),
+            Ok(ClientMessage::ForceLeave) => (),
+            Ok(ClientMessage::GameTypes) => {
+                self.socket
+                    .send_ser(ServerMessage::AvailableGames(
+                        REGISTRY.keys().copied().collect(),
+                    ))
+                    .await;
+            }
+            Ok(ClientMessage::Games) => (),
+            Ok(ClientMessage::Ingame { data }) => {}
+            Ok(ClientMessage::Join { game }) => (),
+            Err(_) => (),
         }
     }
 }
@@ -101,7 +68,7 @@ async fn srv_main(address: String) {
 
     while let Ok((sock, _addr)) = listener.accept().await {
         tokio::spawn(async {
-            let sock = PartialConnection(tungsten::accept_async(sock).await?);
+            let sock = Connection(tungsten::accept_async(sock).await?);
             match sock.upgrade().await {
                 Ok(mut player) => {
                     player.enter().await;
